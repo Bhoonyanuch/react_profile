2bf2ff0ba0cca95c6d1c951132573ae530ef6a67
Bhoonyanu_24 <Bhoonyanuch.ra.66@ubu.ac.th>
2023-07-04 12:26:45 +0700

    first commit

---
+++ src/Tar.js
@@ -0,0 +1,25 @@
+import logo from './nadear.jpg';
+
+function Tar() {
+  return (
+    <>
+      <center>
+        <p>นางสาวปุุณยนุช รัตนศรี</p>
+        <p>รหัสนักศึกษา 66114640637</p>
+        <img src={logo} alt='' />
+        <footer>
+           <ul>
+            <li>Gmail: Bhoonyanuch.ra.66@ubu.ac.th</li>
+            <li>Facebook:Idia Rattanasri</li>
+            </ul> 
+        </footer>
+      </center>
+    </>
+  );
+}
+
+function age() {
+    return 42;
+}
+
+export default Tar
